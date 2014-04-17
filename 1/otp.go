1a279a78fae8fa0b27883f1a7476722d77ef3607
Luchs <Luchs@euirc.eu>
2014-04-18 00:46:23 +0200

    Add wavify command

--- 1/otp.go
+++ 1/otp.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -168,6 +171,68 @@ func cmdXor(args []string) {
 	}
 }
 
+// Writes a new file, overwriting the begin to be a WAVE header.
+// See http://www.sonicspot.com/guide/wavefiles.html for details on
+// the header format.
+func cmdWavify(args []string) {
+	if len(args) != 1 {
+		fmt.Println("Usage: otp wavify <file>")
+		return
+	}
+	file, length, err := openAndGetFileSize(args[0])
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	outfile, err := os.Create(fmt.Sprintf("%s.wav", args[0]))
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	// Create a header using a buffer where writes can't fail.
+	header := new(bytes.Buffer)
+	header.Write([]byte(`RIFF`))
+	binary.Write(header, binary.LittleEndian, uint32(length - 8))
+	header.Write([]byte(`WAVE`))
+	// fmt header
+	header.Write([]byte(`fmt `))
+	// fmt chunk size
+	binary.Write(header, binary.LittleEndian, uint32(16))
+	// fmt Compression code
+	binary.Write(header, binary.LittleEndian, uint16(1))
+	// fmt Number of channels
+	binary.Write(header, binary.LittleEndian, uint16(2))
+	// fmt Sample rate
+	binary.Write(header, binary.LittleEndian, uint32(0x5622))
+	// fmt Average bytes per second
+	binary.Write(header, binary.LittleEndian, uint32(0x15888))
+	// fmt Block align
+	binary.Write(header, binary.LittleEndian, uint16(4))
+	// fmt Significant bits per sample
+	binary.Write(header, binary.LittleEndian, uint16(0x10))
+	// data header
+	header.Write([]byte(`data`))
+	// data chunk size
+	binary.Write(header, binary.LittleEndian, uint32(length - int64(header.Len())))
+
+	// Write the header.
+	headerLen, err := header.WriteTo(outfile)
+	if err != nil {
+		panic(err)
+	}
+	// Skip the original file's header.
+	_, err = file.Seek(headerLen, 0)
+	if err != nil {
+		panic(err)
+	}
+	// Copy all remaining data into the wave file.
+	_, err = io.Copy(outfile, file)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	cmd := ""
 	if len(os.Args) >= 2 {
@@ -178,9 +243,11 @@ func main() {
 		cmdEncrypt(os.Args[2:])
 	case "xor":
 		cmdXor(os.Args[2:])
+	case "wavify":
+		cmdWavify(os.Args[2:])
 	default:
 		fmt.Println("Usage: otp <command>")
-		fmt.Println("Possible commands: encrypt, xor")
+		fmt.Println("Possible commands: encrypt, xor, wavify")
 		os.Exit(1)
 	}
 }
